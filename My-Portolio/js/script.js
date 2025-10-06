539133eb2e1465d6e84a3272fb0d15662c339244
Karen <karen0nogueira@gmail.com>
2025-10-06 17:32:17 -0300

    Melhora do toggle 'Sobre' e corrige possíveis erros de seleção de elementos

--- My-Portolio/js/script.js
+++ My-Portolio/js/script.js
@@ -1,39 +1,11 @@
-document.addEventListener('DOMContentLoaded', () => {
-
-    const mobileMenuButton = document.querySelector('#mobile-menu-button');
-    const mobileMenu = document.querySelector('#mobile-menu');
-
-    if (mobileMenuButton && mobileMenu) {
-        mobileMenuButton.addEventListener('click', () => {
-            mobileMenu.classList.toggle('show-mobile-menu');
-        });
-    }
-
-    const menuBtn = document.querySelector('#menu-btn');
-    const dropdownContent = document.querySelector('.dropdown-content');
-
-    if(menuBtn && dropdownContent) {
-        menuBtn.addEventListener('click',(event) => {
-            event.stopPropagation();
-            dropdownContent.classList.toggle('show');
-        });
-
-        document.addEventListener('click', (event) => {
-            if(!menuBtn.contains(event.target) && !dropdownContent.contains(event.target)) {
-                dropdownContent.classList.remove('show');
-            }
-        });
-    }
-
-    const mobileNavLinks = document.querySelectorAll('.mobile-nav-link, .mobile-nav-link-contact');
-
-    mobileNavLinks.forEach(link => {
-        link.addEventListener('click', () => {
-            if (mobileMenu) {
-                mobileMenu.classList.remove('show-mobile-menu');
-            }
-        });
+document.addEventListener("DOMContentLoaded", () => {
+  const toggleBtn = document.querySelector("#about-Toggle-Btn");
+  const aboutContent = document.querySelector("#about-content");
+  const chevronIcon = toggleBtn.querySelector("i");
+  if (toggleBtn && aboutContent) {
+    toggleBtn.addEventListener("click", () => {
+      aboutContent.classList.toggle("about-content-hidden");
+      toggleBtn.classList.toggle("rotated");
     });
+  }
 });
-
-console.log()
